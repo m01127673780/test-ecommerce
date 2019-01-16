6e493eb946337e7409d4f28a81c3bc5630ae1bc2
Mohamed Elsaeid <m01127673780@gmail.com>
2019-01-16 05:11:55 +0200

    delet test html

--- app/Model/City.php
+++ app/Model/City.php
@@ -5,11 +5,14 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 
 class City extends Model {
-	protected $table    = 'cities';
+	protected $table    = 'cities'; //after Update
 	protected $fillable = [
 		'city_name_ar',
 		'city_name_en',
 		'country_id',
 	];
-
+public function country_id(){
+	teturn this ->hasOne(\App\Model\Country::class,'id','country_id');
 }
+}
+
